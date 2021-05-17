b1f85b6ae4b0c4f63d5cd6901aa3029c10594179
SandroScheuermann <sandroryze@gmail.com>
2021-05-17 17:49:39 -0300

    Add project files.

---
+++ ValidaData/Data.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Data
+{
+    class Data
+    {
+        string[] dataArray;
+        string data;
+        public Data(string data)
+        {
+            this.data = data;
+            dataArray = data.Split('/');
+            Validar();
+        }
+        public void Validar()
+        {
+            try
+            {
+                int dia = 0;
+                int mes = 0;
+                int ano = 0;
+                bool ehBissexto = false;
+
+                if (dataArray.Length < 3)
+                    throw new Exception("Formato de data inválido!\n");
+                else
+                {
+                    dia = Convert.ToInt32(dataArray[0]);
+                    mes = Convert.ToInt32(dataArray[1]);
+                    ano = Convert.ToInt32(dataArray[2]);
+                }
+
+                if (ano % 400 == 0 || (ano % 4 == 0 && ano % 100 != 0))
+                {
+                    ehBissexto = true;
+                }
+
+                if (dia < 0 || dia > 31 || mes == 4 && dia > 32 || mes == 6 && dia > 32 || mes == 9 && dia > 32 || mes == 11 && dia > 32 || mes == 2 && dia > 29 && ehBissexto == true || mes == 2 && dia > 28 && ehBissexto == false)
+                    throw new Exception("Dia Inválido\n");
+
+                if (mes < 0 || mes > 12)
+                    throw new Exception("Mês inválido\n");
+
+                if (ano < 0)
+                    throw new Exception("Ano inválido\n");
+
+                Console.WriteLine(data + "\n");
+            }
+            catch (Exception dataInvalida)
+            {
+                Console.WriteLine(dataInvalida.Message);
+            }
+        }
+    }
+}
---
+++ ValidaData/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string data;
+            Data validaData;
+
+            data = "20/02/2020";
+            validaData = new Data(data);
+
+            data = "29/02/2020";
+            validaData = new Data(data);
+
+            data = "29/02/2021";
+            validaData = new Data(data);
+
+            data = "29/20/2021412";
+            validaData = new Data(data);
+
+            data = "02/2021";
+            validaData = new Data(data);
+
+            data = "teste";
+            validaData = new Data(data);
+
+            data = "sd/02/2021";
+            validaData = new Data(data);
+
+            data = "50/03/2021";
+            validaData = new Data(data);
+
+            Console.ReadLine();
+        }
+    }
+}
